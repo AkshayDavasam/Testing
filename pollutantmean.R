00e1820b89fac043a665ae4b0717e841508e860b
AkshayDavasam <akshay.dr1@gmail.com>
2014-12-22 13:47:26 +0530

    Added New R Code
    Related to calculating pollution mean

---
+++ pollutantmean.R
@@ -0,0 +1,14 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+	files_list <- list.files(directory, full.names = TRUE) #creates a list of files
+	dat <- data.frame()                #creates an empty data frame
+	
+	for (i in id) {
+		                               # loops through the files, rbinding them together
+		dat <- rbind(dat, read.csv(files_list[i]))
+	}
+	
+	dat_subset <- dat[[ pollutant]]   #subsets the column that match the ' pollutant' argument
+	mean(dat_subset, na.rm = TRUE)     #identifies the median of pollutant level
+	                                   # while stripping out the NAs
+}
